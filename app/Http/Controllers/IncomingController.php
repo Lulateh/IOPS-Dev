fe34d4bcf788c9f7b112ea2f1bb7bc03e7f53a88
Alpzar <andresalpizar53@yahoo.es>
2024-09-12 19:12:54 -0600

    Create IncomingController.php
    Se crea el controlador para la vista de Incoming

---
+++ app/Http/Controllers/IncomingController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class IncomingController extends Controller
+{
+    public function sales()
+    {
+        
+        return view('incoming');
+    }
+}
