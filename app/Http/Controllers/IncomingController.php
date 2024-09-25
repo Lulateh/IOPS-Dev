69edc751f2aa3d5cc126ae8ac4112602915e99e4
usuario <c.alexismiranda.p@gmail.com>
2024-09-25 00:26:49 -0600

    cantidad dinamica de entrada a producto

--- app/Http/Controllers/IncomingController.php
+++ app/Http/Controllers/IncomingController.php
@@ -50,11 +50,24 @@ class IncomingController extends Controller
 
     public function updateIncoming(Request $request){
         $existingIncoming = Incoming::find(1);
+        $product = Product::find($request -> prod_id);
         if($existingIncoming){
             $existingIncoming -> producto_id = $request -> prod_id;
             $existingIncoming -> cantidad_entrada = $request -> cantidad;
             $existingIncoming -> proveedor_id = $request -> prov_id;
             $existingIncoming -> save();
+            
+            if($product){
+                $allIncomings = Incoming::all();
+                $total = 0;
+                foreach($allIncomings as $incoming){
+                    if($incoming -> producto_id == $request -> prod_id){
+                        $total += $incoming -> cantidad_entrada;
+                    }
+                }
+                $product->cantidad_stock = $total;
+                $product->save();
+            }
             return redirect(route('incoming'));
         }else{
             return redirect(route('incoming')) -> with("error", "Ha ocurrido un error");
