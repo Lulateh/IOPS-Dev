ca7eca532fbd20688fb4c5adc75bf2b8c071bb23
Deinno <deinnervgzgui@gmail.com>
2024-09-26 21:07:25 -0600

    Update incomingDetail.blade.php
    devolver la vista despues del error en merge

--- resources/views/incomingDetail.blade.php
+++ resources/views/incomingDetail.blade.php
@@ -22,37 +22,6 @@
             <div class =" text-center justify-center ">
                 
                 <h1 class="text-4xl font-bold mb-4">{{ $incoming->product->nombre }}</h1>
-<header class="bg-secondary-green py-2">
-    <div class="columns-2">
-        <div>
-          <a href="{{ route('home') }}"><img class="w-[6rem] ml-20" src= "{{ asset('img/LogiStockIconWhite.png') }}" alt="">
-          </a>
-        </div>
-  
-        <div class="relative float-right mr-20 mt-3">
-          <a href="{{ route('profile') }}"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" class="bi bi-person-circle stroke-cream-10 fill-main-green" viewBox="0 0 16 16">
-                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                </svg>
-              </a>
-          </div>
-  
-    </div> 
-  </header>
-
-    
-<body class="bg-main-green"> 
-    <div class="mt-8 mb-4">
-        <a href="{{ route('incoming') }}" class="ml-20 text-white font-Coda hover:underline text-4xl">  
-            ← Volver
-        </a>
-    </div>
-    <div class="mt-6 max-w-6xl mx-auto bg-white bg-opacity-25 rounded-lg shadow-lg p-10 mb-28">
-        <div class="grid grid-cols-2 gap-8 ">
-            <!-- Left Side (Product Details) -->
-            <div>
-                <!-- Product Name -->
-                <h1 class="text-4xl font-bold mb-4">Nombre del Producto</h1>
 
                
                 <p class="text-xl mb-2">Codigo : {{ $incoming->product->id }}</p>
@@ -89,11 +58,12 @@
             </div>
         </div>
     </div>
-</body>
+</div>
 
 
 
-<div id="deleteModal" class="fixed inset-0 z-50 items-center justify-center bg-black bg-opacity-50 hidden">
+
+<div id="deleteModal" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 hidden">
     
     <div class="bg-white rounded-lg p-6 max-w-md w-full">
         <h2 class="text-lg font-semibold mb-4">Estas a punto de eliminar una entrega de forma definitiva
