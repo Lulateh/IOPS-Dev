f85750fa40ca0ecf9d68f702f156d34b37a44db9
Alpzar <andresalpizar53@yahoo.es>
2024-11-19 19:11:13 -0600

    Responsive
    Responsive agregar y editar entrada

--- resources/views/entradas/add_incoming.blade.php
+++ resources/views/entradas/add_incoming.blade.php
@@ -13,8 +13,8 @@
     </a>
 </div>
    
-<div class= "mt-10 flex justify-center">
-    <div class="bg-lilac bg-opacity-20 w-3/4 p-10 rounded-lg shadow-lg">
+<div class= "mt-10 flex justify-center ">
+    <div class="bg-lilac bg-opacity-20 lg:w-3/4 md:w-3/4 p-10 rounded-lg shadow-lg">
         <h1 class="font-Poppins font-medium text-center text-3xl mb-10 text-main-blue">Agregar Entrada</h1>
         <form action="{{route('incoming.post')}}" method="POST">
             @csrf
@@ -26,7 +26,7 @@
                      <path d="M3 4.5a.5.5 0 0 1 1 0v7a.5.5 0 0 1-1 0zm2 0a.5.5 0 0 1 1 0v7a.5.5 0 0 1-1 0zm2 0a.5.5 0 0 1 1 0v7a.5.5 0 0 1-1 0zm2 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 1 0v7a.5.5 0 0 1-1 0z"/>
                     </svg>Producto</label>
                     <div class="relative inline-block mb-5">
-                        <select name="prod_id" class="block appearance-none w-[31.6rem] text-center border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:shadow-outline" style="background-color: rgb(255, 255, 255);">
+                        <select name="prod_id" class="md:w-[16rem] block appearance-none lg:w-[31.6rem] text-center border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:shadow-outline" style="background-color: rgb(255, 255, 255);">
                             <option value="">Seleccione un producto</option>
                             @foreach($posts as $producto)
                             <option value="{{ $producto->id }}" {{ old('prod_id') == $producto->id ? 'selected' : '' }}>{{ $producto->nombre }} ({{ $producto->cantidad_stock }})</option>
@@ -46,7 +46,7 @@
                     </svg>
                 Proveedor</label>
                     <div class="relative inline-block  mb-5">
-                        <select name="prov_id" class="block appearance-none w-[31.6rem] text-center border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:shadow-outline" style="background-color: rgb(255, 255, 255);">
+                        <select name="prov_id" class="block appearance-none lg:w-[31.6rem] text-center border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:shadow-outline md:w-[16rem]" style="background-color: rgb(255, 255, 255);">
                             <option value="">Seleccione un proveedor</option>
                             @foreach($proveedores as $proveedor)
                             @if($proveedor->estado == 'activo')
@@ -62,7 +62,7 @@
                 </div>
                 
             </div>
-            <div class="ml-[16rem]">
+            <div class="lg:ml-[16rem] md:items-center">
                 <label class="flex items-center mt-9 bg- mb-2 font-semibold text-xl"> 
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="mr-4" viewBox="0 0 16 16">
                     <path d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434zM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567zM7.5 9.933l-2.75 1.571v3.134l2.75-1.571zm1 3.134 2.75 1.571v-3.134L8.5 9.933zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567zm2.242-2.433V3.504L8.5 5.076V8.21zM7.5 8.21V5.076L4.75 3.504v3.134zM5.258 2.643 8 4.21l2.742-1.567L8 1.076zM15 9.933l-2.75 1.571v3.134L15 13.067zM3.75 14.638v-3.134L1 9.933v3.134z"/>
--- resources/views/entradas/incomingEdit.blade.php
+++ resources/views/entradas/incomingEdit.blade.php
@@ -23,7 +23,7 @@
                 <div>
                     <label class="block mb-2 font-semibold">Código del producto</label>
                     <div class="relative inline-block mb-5">
-                        <select name ="prod_id" class="block appearance-none w-[31.6rem] text-center border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:shadow-outline" style="background-color: rgb(255, 255, 255);">
+                        <select name ="prod_id" class="md:w-[16rem] block appearance-none lg:w-[31.6rem] text-center border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:shadow-outline" style="background-color: rgb(255, 255, 255);">
                             @foreach($posts as $producto)
                             <option value="{{ $producto->id }}" {{ $incoming->producto_id == $producto->id ? 'selected' : '' }}>{{ $producto->nombre }}</option>
                             @endforeach
@@ -38,7 +38,7 @@
                 <div>
                     <label class="block mb-2 font-semibold">Proveedor</label>
                     <div class="relative inline-block  mb-5">
-                        <select name ="prov_id" class="block appearance-none w-[31.6rem] text-center border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:shadow-outline" style="background-color: rgb(255, 255, 255);">
+                        <select name ="prov_id" class="md:w-[16rem] block appearance-none lg:w-[31.6rem] text-center border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:shadow-outline" style="background-color: rgb(255, 255, 255);"">
                             @foreach($proveedores as $proveedor)
                               <option value="{{ $proveedor->id }}" {{ $incoming->proveedor_id == $proveedor->id ? 'selected' : '' }}>{{ $proveedor->nombre_proveedor}}</option>
                             @endforeach
@@ -51,7 +51,7 @@
                 </div>
                 
             </div>
-            <div class="ml-[16rem]">
+            <div class="lg:ml-[16rem] md:items-center">
                 <label class="block mt-9 mb-2 font-semibold">Cantidad del producto</label>
                 <input type="number" name="cantidad" class="w-[31.6rem] p-2 mb-4 rounded-lg text-black" style="background-color: rgb(255, 255, 255);" required>
             @error('cantidad')
